1c5fdcb3ace36b212d0c157f203bdd0fa7327372
Anibal89 <72290798+Anibal89@users.noreply.github.com>
2022-07-15 08:24:02 -0600

    Actualizacion - template v2
    Configuracion - plantilla

--- src/Components/Preloader.js
+++ src/Components/Preloader.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import IconoCarga from '../Img/Icono-menu.png';
 
 function Preloader() {
   return (
    <div className="preloader flex-column justify-content-center align-items-center">
-  <img className="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height={60} width={60} />
+  <img className="animation__shake" src={IconoCarga} alt="UnisaLogo" height={60} width={60} />
 </div>
 
   )
